479943ddfea6e75b0f9146e33df3454c3a0f7b9e
Queen Ameh <queenochanya58@gmail.com>
2022-01-06 10:34:52 +0100

    add app colors and global spacing

---
+++ resavation/lib/ui/shared/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF2003D0);
+const Color kPrimaryLightColor = Color(0xFFB3A7F9);
+const Color kRed = Color(0xFFDB3535);
+const Color kBlack = Color(0xFF080B51);
+const Color kDarkBlue = Color(0xFF407BFF);
+const Color kGreen = Color(0xFF28C16A);
+const Color kPurple = Color(0xFFAD33Ec);
