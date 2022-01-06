c69c02710e052010412fc431e2508a26c2b55a86
Queen Ameh <queenochanya58@gmail.com>
2022-01-06 18:28:29 +0100

    implemented bottom navigation bar
    This implements a bottom navigator bar with page transition switcher using animation.

--- resavation/lib/ui/views/main/main_view.dart
+++ resavation/lib/ui/views/main/main_view.dart
@@ -1,5 +1,11 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:resavation/ui/shared/colors.dart';
+import 'package:resavation/ui/views/favorite/favorite_view.dart';
+import 'package:resavation/ui/views/home/home_view.dart';
 import 'package:resavation/ui/views/main/main_viewmodel.dart';
+import 'package:resavation/ui/views/search/search_view.dart';
+import 'package:resavation/ui/views/settings/settings_view.dart';
 import 'package:stacked/stacked.dart';
 
 class MainView extends StatelessWidget {
@@ -9,11 +15,66 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<MainViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: Text('Main View'),
+        body: PageTransitionSwitcher(
+            duration: const Duration(milliseconds: 300),
+            reverse: model.reverse,
+            transitionBuilder: (
+              Widget child,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+            ) {
+              return SharedAxisTransition(
+                child: child,
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                transitionType: SharedAxisTransitionType.horizontal,
+              );
+            },
+            child: getViewForIndex(model.currentIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: kPrimaryColor,
+          iconSize: 28,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: model.currentIndex,
+          onTap: model.setIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: '',
+            ),
+          ],
         ),
       ),
       viewModelBuilder: () => MainViewModel(),
     );
+  }
+}
+
+Widget getViewForIndex(int index) {
+  switch (index) {
+    case 0:
+      return HomeView();
+    case 1:
+      return FavoriteView();
+    case 2:
+      return SearchView();
+    case 3:
+      return SettingsView();
+    default:
+      return HomeView();
   }
 }
--- resavation/lib/ui/views/main/main_viewmodel.dart
+++ resavation/lib/ui/views/main/main_viewmodel.dart
@@ -1,3 +1,3 @@
 import 'package:stacked/stacked.dart';
 
-class MainViewModel extends BaseViewModel {}
+class MainViewModel extends IndexTrackingViewModel {}
