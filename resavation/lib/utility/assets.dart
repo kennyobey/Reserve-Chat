ff1ba1bdfbd1da2139776b5c3589ec272147527e
Queen Ameh <queenochanya58@gmail.com>
2022-01-07 14:21:35 +0100

    setup assets

---
+++ resavation/lib/utility/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+  static const onboarding1 = 'assets/placeholders/onboarding1.svg';
+  static const onboarding2 = 'assets/placeholders/onboarding2.svg';
+  static const onboarding3 = 'assets/placeholders/onboarding3.svg';
+}
