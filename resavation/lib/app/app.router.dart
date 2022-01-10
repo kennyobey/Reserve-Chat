3e20da79ca3dcae671858020acb1e5ea3a687b14
Queen Ameh <queenochanya58@gmail.com>
2022-01-10 19:21:29 +0100

    register views

--- resavation/lib/app/app.router.dart
+++ resavation/lib/app/app.router.dart
@@ -6,23 +6,30 @@
 
 // ignore_for_file: public_member_api_docs
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked/stacked_annotations.dart';
 
+import '../ui/views/login/login_view.dart';
 import '../ui/views/main/main_view.dart';
 import '../ui/views/onboarding/onboarding_view.dart';
+import '../ui/views/rest_password/reset_password_view.dart';
+import '../ui/views/signup/signup_view.dart';
 import '../ui/views/startup/startup_view.dart';
 
 class Routes {
   static const String startupView = '/';
   static const String mainView = '/main-view';
   static const String onboardingView = '/onboarding-view';
+  static const String signUpView = '/sign-up-view';
+  static const String logInView = '/log-in-view';
+  static const String resetPasswordView = '/reset-password-view';
   static const all = <String>{
     startupView,
     mainView,
     onboardingView,
+    signUpView,
+    logInView,
+    resetPasswordView,
   };
 }
 
@@ -33,25 +40,46 @@ class StackedRouter extends RouterBase {
     RouteDef(Routes.startupView, page: StartupView),
     RouteDef(Routes.mainView, page: MainView),
     RouteDef(Routes.onboardingView, page: OnboardingView),
+    RouteDef(Routes.signUpView, page: SignUpView),
+    RouteDef(Routes.logInView, page: LogInView),
+    RouteDef(Routes.resetPasswordView, page: ResetPasswordView),
   ];
   @override
   Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
   final _pagesMap = <Type, StackedRouteFactory>{
     StartupView: (data) {
-      return MaterialPageRoute<dynamic>(
+      return buildAdaptivePageRoute<dynamic>(
         builder: (context) => const StartupView(),
         settings: data,
       );
     },
     MainView: (data) {
-      return CupertinoPageRoute<dynamic>(
+      return buildAdaptivePageRoute<dynamic>(
         builder: (context) => const MainView(),
         settings: data,
       );
     },
     OnboardingView: (data) {
-      return CupertinoPageRoute<dynamic>(
+      return buildAdaptivePageRoute<dynamic>(
         builder: (context) => const OnboardingView(),
+        settings: data,
+      );
+    },
+    SignUpView: (data) {
+      return buildAdaptivePageRoute<dynamic>(
+        builder: (context) => const SignUpView(),
+        settings: data,
+      );
+    },
+    LogInView: (data) {
+      return buildAdaptivePageRoute<dynamic>(
+        builder: (context) => const LogInView(),
+        settings: data,
+      );
+    },
+    ResetPasswordView: (data) {
+      return buildAdaptivePageRoute<dynamic>(
+        builder: (context) => const ResetPasswordView(),
         settings: data,
       );
     },
