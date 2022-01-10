053d1b7a87812825ab9ce6b4b0594015ea229a18
Queen Ameh <queenochanya58@gmail.com>
2022-01-10 19:24:29 +0100

    implemented reset password view

---
+++ resavation/lib/ui/views/rest_password/reset_password_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:resavation/ui/shared/dump_widgets/resavation_button.dart';
+import 'package:resavation/ui/shared/dump_widgets/resavation_textfield.dart';
+import 'package:resavation/ui/shared/dump_widgets/signup_text.dart';
+import 'package:resavation/ui/shared/spacing.dart';
+import 'package:resavation/ui/shared/text_styles.dart';
+import 'package:resavation/ui/views/rest_password/reset_password_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+
+class ResetPasswordView extends StatelessWidget {
+  const ResetPasswordView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<ResetPasswordViewModel>.reactive(
+      builder: (context, model, child) => SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                verticalSpaceLarge,
+                Center(
+                  child: Text(
+                    'Reset Password ',
+                    style: AppStyle.kHeading1,
+                  ),
+                ),
+                verticalSpaceMedium,
+                Text(
+                  "Enter the e-mail address associated with the account. We’ll e-mail a link to reset your password.",
+                  style: AppStyle.kBodyRegular,
+                ),
+                verticalSpaceLarge,
+                Text(
+                  'Email',
+                  style: AppStyle.kBodyRegular,
+                ),
+                ResavationTextField(
+                  hintText: 'Email',
+                ),
+                verticalSpaceMedium,
+                ResavationButton(
+                  title: 'Send Reset Link',
+                  onTap: model.goToMainView,
+                ),
+                verticalSpaceMassive,
+                SignUpText(
+                  onTap: model.goToSignUpView,
+                ),
+                verticalSpaceMassive,
+              ],
+            ),
+          ),
+        ),
+      ),
+      viewModelBuilder: () => ResetPasswordViewModel(),
+    );
+  }
+}
