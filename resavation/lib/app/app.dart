f23c3512707455223edf73a85b67c5704a019380
Queen Ameh <queenochanya58@gmail.com>
2022-01-10 19:21:29 +0100

    register views

--- resavation/lib/app/app.dart
+++ resavation/lib/app/app.dart
@@ -1,14 +1,21 @@
+import 'package:resavation/ui/views/home/home_view.dart';
+import 'package:resavation/ui/views/login/login_view.dart';
 import 'package:resavation/ui/views/main/main_view.dart';
 import 'package:resavation/ui/views/onboarding/onboarding_view.dart';
+import 'package:resavation/ui/views/rest_password/reset_password_view.dart';
+import 'package:resavation/ui/views/signup/signup_view.dart';
 import 'package:resavation/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 @StackedApp(
   routes: [
-    MaterialRoute(page: StartupView, initial: true),
-    CupertinoRoute(page: MainView),
-    CupertinoRoute(page: OnboardingView),
+    AdaptiveRoute(page: StartupView, initial: true),
+    AdaptiveRoute(page: MainView),
+    AdaptiveRoute(page: OnboardingView),
+    AdaptiveRoute(page: SignUpView),
+    AdaptiveRoute(page: LogInView),
+    AdaptiveRoute(page: ResetPasswordView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
