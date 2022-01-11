74e4f6a7eb539cabceae345fccb44b2a8aedd1da
Queen Ameh <queenochanya58@gmail.com>
2022-01-11 16:59:40 +0100

    add app icon in svg image and demo network images

--- resavation/lib/utility/assets.dart
+++ resavation/lib/utility/assets.dart
@@ -1,5 +1,13 @@
 class Assets {
+  static const logo = 'assets/icons/app_icon.svg';
   static const onboarding1 = 'assets/placeholders/onboarding1.svg';
   static const onboarding2 = 'assets/placeholders/onboarding2.svg';
   static const onboarding3 = 'assets/placeholders/onboarding3.svg';
+  static const profile_image = 'assets/images/profile_image.png';
+  static const house_placeholder = 'https://image.shutterstock.com/image-photo/beautiful-contemporary-white-house-lush-600w-1911831754.jpg';
+  static const office_placeholder = 'https://image.shutterstock.com/image-photo/buildings-skyline-manhattan-new-york-600w-1934264801.jpg';
+  static const kitchen_placeholder = 'https://image.shutterstock.com/image-photo/kitchen-interior-island-sink-cabinets-600w-325910156.jpg';
+  static const lagos_location_placeholder = 'https://image.shutterstock.com/image-photo/lagos-nigeria-skyline-dawn-600w-1055844236.jpg';
+  static const abuja_location_placeholder = 'https://image.shutterstock.com/image-photo/zuma-rock-large-monolith-igneous-600w-1022345497.jpg';
+  static const kaduna_location_placeholder = 'https://image.shutterstock.com/image-photo/29th-september-2019-kaduna-nigeria-600w-1521109949.jpg';
 }
