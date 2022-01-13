9d70169c359e19cc65310def3826d724c1a1f994
Queen Ameh <queenochanya58@gmail.com>
2022-01-13 15:58:48 +0100

    Implement favorite view

--- resavation/lib/ui/views/favorite/favorite_view.dart
+++ resavation/lib/ui/views/favorite/favorite_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:resavation/ui/shared/dump_widgets/favorite_card.dart';
+import 'package:resavation/ui/shared/dump_widgets/properties_sort.dart';
+import 'package:resavation/ui/shared/spacing.dart';
 import 'package:resavation/ui/shared/text_styles.dart';
 import 'package:resavation/ui/views/favorite/favorite_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -9,11 +12,51 @@ class FavoriteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<FavoriteViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: Text(
-            'Favorite View',
-            style: AppStyle.kHeading1,
+      builder: (context, model, child) => SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Favorite',
+                      style: AppStyle.kHeading1,
+                    ),
+                    verticalSpaceMedium,
+                    SortProperty(
+                      noOfProperties: 30,
+                      sortByTitle: 'Date',
+                    ),
+                  ],
+                ),
+                verticalSpaceMedium,
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        for (final property in model.properties) ...[
+                          FavoriteCard(
+                            image: property.image,
+                            amountPerYear: property.amountPerYear,
+                            location: property.location,
+                            address: property.address,
+                            numberOfBathrooms: property.numberOfBedrooms,
+                            numberOfBedrooms: property.numberOfBathrooms,
+                            squareFeet: property.squareFeet,
+                            isFavoriteTap: property.isFavoriteTap,
+                            category: property.category,
+                          ),
+                          verticalSpaceSmall,
+                        ]
+                      ],
+                    ),
+                  ),
+                ),
+                verticalSpaceMedium,
+              ],
+            ),
           ),
         ),
       ),
--- resavation/lib/ui/views/favorite/favorite_viewmodel.dart
+++ resavation/lib/ui/views/favorite/favorite_viewmodel.dart
@@ -1,3 +1,6 @@
+import 'package:resavation/model/property_model.dart';
 import 'package:stacked/stacked.dart';
 
-class FavoriteViewModel extends BaseViewModel {}
+class FavoriteViewModel extends BaseViewModel {
+  List<Property> get properties => ListOfProperties;
+}
