8c9f668d8be595f4a061fa652e8a273a62b1f972
Queen Ameh <queenochanya58@gmail.com>
2022-01-14 21:57:03 +0100

    setup navigation for all added views

--- resavation/lib/app/app.dart
+++ resavation/lib/app/app.dart
@@ -1,8 +1,16 @@
-import 'package:resavation/ui/views/home/home_view.dart';
+import 'package:resavation/ui/views/booking_submission/booking_submission_view.dart';
+import 'package:resavation/ui/views/confirmation/confirmation_view.dart';
+import 'package:resavation/ui/views/date_picker/date_picker_view.dart';
+import 'package:resavation/ui/views/filter/filter_view.dart';
 import 'package:resavation/ui/views/login/login_view.dart';
 import 'package:resavation/ui/views/main/main_view.dart';
+import 'package:resavation/ui/views/map/map_view.dart';
 import 'package:resavation/ui/views/onboarding/onboarding_view.dart';
+import 'package:resavation/ui/views/payment/payment_view.dart';
+import 'package:resavation/ui/views/property_details/property_details_view.dart';
+import 'package:resavation/ui/views/property_owner_profile/property_owner_profile_view.dart';
 import 'package:resavation/ui/views/rest_password/reset_password_view.dart';
+import 'package:resavation/ui/views/settings/settings_view.dart';
 import 'package:resavation/ui/views/signup/signup_view.dart';
 import 'package:resavation/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
@@ -16,6 +24,15 @@ import 'package:stacked_services/stacked_services.dart';
     AdaptiveRoute(page: SignUpView),
     AdaptiveRoute(page: LogInView),
     AdaptiveRoute(page: ResetPasswordView),
+    AdaptiveRoute(page: FilterView),
+    AdaptiveRoute(page: DatePickerView),
+    AdaptiveRoute(page: PropertyDetailsView),
+    AdaptiveRoute(page: BookingSubmissionView),
+    AdaptiveRoute(page: SettingsView),
+    AdaptiveRoute(page: MapView),
+    AdaptiveRoute(page: PaymentView),
+    AdaptiveRoute(page: PropertyOwnerProfileView),
+    AdaptiveRoute(page: ConfirmationView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
