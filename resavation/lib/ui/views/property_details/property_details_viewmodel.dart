ec98b317c196e591972eb16519d2a89760e02b63
Queen Ameh <queenochanya58@gmail.com>
2022-01-21 20:45:03 +0100

    Implement property details view

--- resavation/lib/ui/views/property_details/property_details_viewmodel.dart
+++ resavation/lib/ui/views/property_details/property_details_viewmodel.dart
@@ -1,3 +1,24 @@
+import 'package:resavation/app/app.locator.dart';
+import 'package:resavation/app/app.router.dart';
+import 'package:resavation/model/amenities_model.dart';
+import 'package:resavation/model/property_model.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class PropertyDetailsViewModel extends BaseViewModel {}
+class PropertyDetailsViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  int _pagePosition = 0;
+  int get pagePosition => _pagePosition;
+
+  void onPageChanged(int index) {
+    _pagePosition = index;
+    notifyListeners();
+  }
+
+  List<Property> get properties => ListOfProperties;
+  List<Amenity> get amenities => ListOfAmenities;
+
+  void navigateBack() {
+    _navigationService.back();
+  }
+}
