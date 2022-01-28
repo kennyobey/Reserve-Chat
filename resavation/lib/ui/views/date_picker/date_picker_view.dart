22707c540793e8e101dfef2199b279b4640ecdf2
Queen Ameh <queenochanya58@gmail.com>
2022-01-28 16:08:49 +0100

    Implement date Picker view

--- resavation/lib/ui/views/date_picker/date_picker_view.dart
+++ resavation/lib/ui/views/date_picker/date_picker_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:resavation/ui/shared/colors.dart';
 import 'package:resavation/ui/shared/dump_widgets/resavation_app_bar.dart';
+import 'package:resavation/ui/shared/dump_widgets/resavation_button.dart';
+import 'package:resavation/ui/shared/spacing.dart';
 import 'package:resavation/ui/shared/text_styles.dart';
 import 'package:resavation/ui/views/date_picker/date_picker_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -14,14 +17,104 @@ class DatePickerView extends StatelessWidget {
         appBar: ResavationAppBar(
           title: "Apply for rent",
         ),
-        body: Center(
-          child: Text(
-            'Data Picker',
-            style: AppStyle.kHeading1,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 15,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      verticalSpaceMedium,
+                      Text(
+                        'check in date',
+                        style: AppStyle.kBodyBold,
+                      ),
+                      verticalSpaceMedium,
+                      CalendarDatePicker(
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2025),
+                        onDateChanged: (_) {},
+                      ),
+                      verticalSpaceMedium,
+                      Text(
+                        'Choose payment',
+                        style: AppStyle.kBodyBold,
+                      ),
+                      PaymentChoiceListTile(
+                        title: 'Monthly',
+                        value: ChoiceOfPayment.Monthly,
+                        subTitle: 'Every month you pay',
+                        trailing: '10,000',
+                      ),
+                      PaymentChoiceListTile(
+                        title: 'Annual',
+                        value: ChoiceOfPayment.Annual,
+                        subTitle: 'Every year you pay',
+                        trailing: '120,000',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ResavationButton(
+                title: 'Continue',
+              ),
+            ],
           ),
         ),
       ),
       viewModelBuilder: () => DatePickerViewModel(),
+    );
+  }
+}
+
+class PaymentChoiceListTile extends ViewModelWidget<DatePickerViewModel> {
+  const PaymentChoiceListTile({
+    required this.trailing,
+    required this.subTitle,
+    required this.title,
+    required this.value,
+  });
+
+  final String title;
+  final String trailing;
+  final ChoiceOfPayment value;
+  final String subTitle;
+
+  @override
+  Widget build(BuildContext context, Model) {
+    return InkWell(
+      onTap: () => Model.onPaymentChoiceChanged(value),
+      splashColor: kGray,
+      child: Row(
+        children: [
+          Expanded(
+            child: ListTile(
+              title: Text(
+                title,
+                style: AppStyle.kBodyBold,
+              ),
+              subtitle: Text(subTitle),
+              leading: Radio<ChoiceOfPayment>(
+                groupValue: Model.paymentChoice,
+                value: value,
+                onChanged: Model.onPaymentChoiceChanged,
+              ),
+            ),
+          ),
+          horizontalSpaceMedium,
+          Text(
+            trailing,
+            style: AppStyle.kBodyBold,
+          ),
+        ],
+      ),
     );
   }
 }
