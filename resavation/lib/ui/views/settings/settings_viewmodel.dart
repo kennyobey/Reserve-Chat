0e3f2f04e3879b7d2c4941d544a2a9d853231725
Kehinde Obe <48479284+kennyobey@users.noreply.github.com>
2022-02-04 10:23:46 +0100

    Property owner page design

--- resavation/lib/ui/views/settings/settings_viewmodel.dart
+++ resavation/lib/ui/views/settings/settings_viewmodel.dart
@@ -1,3 +1,27 @@
+import 'package:resavation/app/app.locator.dart';
+import 'package:resavation/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class SettingsViewModel extends BaseViewModel {}
+class SettingsViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  bool _notificationSwitchValue = false;
+  bool get notificationSwitchValue => _notificationSwitchValue;
+
+  void onNotificationSwitchChanged(bool? value) {
+    _notificationSwitchValue = value!;
+    notifyListeners();
+  }
+
+  bool _appNotificationSwitchValue = false;
+  bool get appNotificationSwitchValue => _appNotificationSwitchValue;
+
+  void onAppNotificationSwitchValue(bool? value) {
+    _appNotificationSwitchValue = value!;
+    notifyListeners();
+  }
+
+  void logout() async {
+    await _navigationService.clearStackAndShow(Routes.onboardingView);
+  }
+}
