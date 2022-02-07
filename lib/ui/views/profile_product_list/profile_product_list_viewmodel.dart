31d5c37d1918daa58661544edf5f2925758b3c82
Ochanya Queen Ameh <81382563+ochyluv@users.noreply.github.com>
2022-02-07 12:06:32 +0100

    Revert "Develop"

--- lib/ui/views/profile_product_list/profile_product_list_viewmodel.dart
+++
@@ -1,6 +0,0 @@
-import 'package:resavation/model/property_model.dart';
-import 'package:stacked/stacked.dart';
-
-class ProfileProductListViewModel extends BaseViewModel {
-  List<Property> get properties => ListOfProperties;
-}
