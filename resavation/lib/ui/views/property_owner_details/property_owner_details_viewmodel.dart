d058d00051eff1598a172876834689fa31f67b1e
Oti Temitope Emmanuel <otitemitope6@gmail.com>
2022-05-13 01:56:58 -0500

    Application Updated

--- resavation/lib/ui/views/property_owner_details/property_owner_details_viewmodel.dart
+++ resavation/lib/ui/views/property_owner_details/property_owner_details_viewmodel.dart
@@ -1,7 +1,7 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:resavation/app/app.locator.dart';
-import 'package:resavation/app/app.router.dart';';
+import 'package:resavation/app/app.router.dart';
 import 'package:resavation/ui/views/property_owner_spaceType/property_owner_spacetype_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
