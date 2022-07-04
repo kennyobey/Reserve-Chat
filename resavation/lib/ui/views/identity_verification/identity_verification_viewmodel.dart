67c93c3439b3f8ef0b962888021a8f75fac91138
Oti Temitope Emmanuel <otitemitope6@gmail.com>
2022-07-04 11:28:35 +0100

    update

--- resavation/lib/ui/views/identity_verification/identity_verification_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-import 'package:resavation/app/app.locator.dart';
-import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
-
-class IdentityVerificationViewModel extends BaseViewModel {
-  final _navigationService = locator<NavigationService>();
-}
